b8bdbae5bc2222024fbb2f3fe1714d9152d05997
asevibes <andrewtimothy2727@gmail.com>
2025-06-17 10:36:25 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Ase Vibes</h1>
+      <p>Welcome to the global music discovery app!</p>
+    </div>
+  );
+}
+
+export default App;
